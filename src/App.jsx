1f73bfd79375bf5385ce7ae1bcc77e2a080db26b
rabbi75 <golamrabbi@nexgencommunication.net>
2025-01-17 10:01:52 +0600

    redux problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import Counter from "./components/Counter";
-import { Provider } from "react-redux";
-import {store} from "./redux/store";
-
 export default function App() {
-
     return (
-      <Provider store={store}>
         <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
               <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
                   Simple Counter Application
@@ -14,6 +9,5 @@ export default function App() {
                 <Counter />
               </div>
         </div>
-      </Provider>
     );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import createStore from "redux";
+import { createStore } from "redux"; // Use named import
 import counterReducer from "./counter/counterReducer";
 
 const store = createStore(counterReducer);
