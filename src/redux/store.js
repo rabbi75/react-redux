f0c131366f79d2d3f33346e0c42cfb31a66aab7a
rabbi75 <golamrabbi@nexgencommunication.net>
2025-01-17 16:38:15 +0600

    middleware add

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,20 @@
-import { createStore } from "redux"; // Use named import
+import { applyMiddleware, createStore } from "redux"; // Use named import
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+// create middleware
+
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+    console.log('upcomming state' + JSON.stringify(upcomingState))
+
+    //pass action
+    return next(action);
+}
+
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
